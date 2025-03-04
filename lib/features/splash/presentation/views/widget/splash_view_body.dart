ee1abd407c4212a3fdd0a090571bd2117a62de7e
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-04 16:37:48 +0200

    refactor init state & navigat to home

--- lib/features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:bookly_app/core/utils/constans.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widget/sliding_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -13,21 +16,16 @@ class _SplashViewBodyState extends State<SplashViewBody>
     with
         SingleTickerProviderStateMixin // هنا استخدمنا single عشان هو انيمشن واحد
 {
-  late AnimationController animationController;
-  late Animation<Offset> slidingAnimation;
+  late AnimationController
+      animationController; // المتحكم الاساسى فى الانمشن بيحدد مدته بيشغله وكدا
+  late Animation<Offset>
+      slidingAnimation; // ده متغير بيخزن حركه النص باستخدام tween هي المسؤله عن تحديد البدايه والنهايه للحركه
 // هنا انيمشن على النص اللى تحت الاللوجو
   @override
   void initState() {
-    //استخدمنا initstate عشان ناكسس ال انيمشن
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 5), end: Offset(0, 0))
-            .animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigatToHome();
   }
 
 // dispose دي عشان لما السبلاش يخلص ميخدش مساحه من الميموري على الفاصي بيقف اول ميخلص
@@ -48,6 +46,29 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
         SlidingAnimation(slidingAnimation: slidingAnimation),
       ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+        vsync:
+            this, // vsync: this معناه إننا بنربط الأنيميشن بـ TickerProvider، وده بيمنع استهلاك موارد الجهاز بدون داعي
+        duration: const Duration(milliseconds: 750));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset(0, 0))
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigatToHome() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.to(() => const HomeView(),
+            transition: Transition.fade, duration: kTransationDuration);
+      },
     );
   }
 }
