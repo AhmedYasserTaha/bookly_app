28759e235dbe0260a6939ce35f18ffc10a0b7151
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-05 00:42:05 +0200

    create styles file and add title to home view

---
+++ lib/core/utils/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const tilteMedium =
+      TextStyle(fontSize: 22, fontWeight: FontWeight.w500);
+}
--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bookly_app/core/utils/style.dart';
 import 'package:bookly_app/features/home/presentation/views/widget/custom_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widget/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widget/featured_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -7,30 +8,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 24),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          const SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.tilteMedium,
+          )
+        ],
+      ),
     );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        height: MediaQuery.of(context).size.height * .3,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: FeaturedListViewItem(),
-            );
-          },
-        ));
   }
 }
