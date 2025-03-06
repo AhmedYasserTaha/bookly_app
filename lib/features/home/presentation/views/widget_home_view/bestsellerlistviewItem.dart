3ac41a84fee27356d0d533548892d763c67df81a
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-07 00:18:49 +0200

    create custom book details item app bar

--- lib/features/home/presentation/views/widget_home_view/bestsellerlistviewItem.dart
+++ lib/features/home/presentation/views/widget_home_view/bestsellerlistviewItem.dart
@@ -1,71 +1,79 @@
 // ignore_for_file: file_names
 
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/core/utils/style.dart';
+import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widget_home_view/book_realting.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.6 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(16),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.KBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.6 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(16),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AssetsData.testImage),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    "Harry Potter \nand the Goblet of Fire",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20,
+            SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      "Harry Potter \nand the Goblet of Fire",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20,
+                    ),
                   ),
-                ),
-                SizedBox(width: 3),
-                Text(
-                  "J.K. Rowling",
-                  style: Styles.textStyle16,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "19.99 €",
-                      style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 3),
-                    // SizedBox(width: 45),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 50),
-                      child: BookRating(),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                  SizedBox(width: 3),
+                  Text(
+                    "J.K. Rowling",
+                    style: Styles.textStyle16,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "19.99 €",
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(width: 3),
+                      // SizedBox(width: 45),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: BookRating(),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
