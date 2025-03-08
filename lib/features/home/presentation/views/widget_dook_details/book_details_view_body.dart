6508412e51b143197ef58fa152ffa476c6363583
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-08 13:10:21 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widget_dook_details/book_details_view_body.dart
+++ lib/features/home/presentation/views/widget_dook_details/book_details_view_body.dart
@@ -12,59 +12,69 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CustomAppBarBookDetails(),
-            Padding(
-              padding: const EdgeInsets.only(top: 40, bottom: 40),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * .50,
-                child: CustomBookImage(),
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            // hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const CustomAppBarBookDetails(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40, bottom: 40),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * .50,
+                      child: CustomBookImage(),
+                    ),
+                  ),
+                  const Text(
+                    "The Jungle Book",
+                    style: Styles.textStyle30,
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Opacity(
+                    opacity: .7,
+                    child: Text(
+                      "Rudyard Kipling",
+                      style: Styles.textStyle18,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  const ButtonAction(),
+                  const Expanded(
+                    child: SizedBox(
+                      height: 50,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "You can also like",
+                      style: Styles.textStyle16
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SmillerBooksListView(),
+                ],
               ),
             ),
-            const Text(
-              "The Jungle Book",
-              style: Styles.textStyle30,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Opacity(
-              opacity: .7,
-              child: Text(
-                "Rudyard Kipling",
-                style: Styles.textStyle18,
-              ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const BookRating(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            const ButtonAction(),
-            const SizedBox(
-              height: 50,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "You can also like",
-                style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            SmillerBooksListView(),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
