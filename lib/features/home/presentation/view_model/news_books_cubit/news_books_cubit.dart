75c4a599804b0b2ceffc7710efdd812a001815df
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-10 13:15:22 +0200

    news books cubit done

--- lib/features/home/presentation/view_model/news_books_cubit/news_books_cubit.dart
+++ lib/features/home/presentation/view_model/news_books_cubit/news_books_cubit.dart
@@ -1,10 +1,26 @@
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book.model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
 part 'news_books_state.dart';
 
 class NewsBooksCubit extends Cubit<NewsBooksState> {
-  NewsBooksCubit() : super(NewsBooksInitial());
+  NewsBooksCubit(this.homeRepo) : super(NewsBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsBooks() async {
+    emit(NewsBooksloading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewsBooksfailure(errMessage: failure.errMessage));
+      },
+      (books) {
+        emit(NewsBooksSuccess(books: books));
+      },
+    );
+  }
 }
