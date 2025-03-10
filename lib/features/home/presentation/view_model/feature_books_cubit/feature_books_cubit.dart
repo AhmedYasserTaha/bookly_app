e305fb0ce7e65976edca939187d8d68ba08d0e43
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-10 13:03:33 +0200

    create feature books methods

--- lib/features/home/presentation/view_model/feature_books_cubit/feature_books_cubit.dart
+++ lib/features/home/presentation/view_model/feature_books_cubit/feature_books_cubit.dart
@@ -1,10 +1,27 @@
 // ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book.model.dart';
-import 'package:dartz/dartz.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 part 'feature_books_state.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBookInitial());
+  FeatureBooksCubit(this.homeRepo) : super(FeatureBookInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks() async {
+    emit(FeatureBookLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(
+          FeatureBookFailure(errMessage: failure.errMessage),
+        );
+      },
+      (books) {
+        emit(FeatureBookSuccess(books: books));
+      },
+    );
+  }
 }
